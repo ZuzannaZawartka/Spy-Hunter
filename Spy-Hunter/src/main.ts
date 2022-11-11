e523ee3022398d2c160f059f2a9b0236d2cb906e
Zuzia <raf.zuzia7@gmail.com>
2022-11-11 16:23:11 +0100

    initial config
    - added graphics
    - trying use canvas

--- Spy-Hunter/src/main.ts
+++ Spy-Hunter/src/main.ts
@@ -1,23 +1,67 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+var ctx = document.getElementById("canvas")!.getContext("2d"),
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  canvasTemp = document.createElement("canvas"),
+  scrollImg = new Image(),
+  car = new Image(),
+  imgWidth = 0,
+  imgHeight = 0,
+  carWidth = 0,
+  carHeight = 0,
+  canvasWidth = 768,
+  canvasHeight = 800,
+  scrollVal = 0,
+  driven = 0,
+  speed = 10,
+  imgScale = 3;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+scrollImg.src = "../graphics/SpyHunterArea01.png";
+scrollImg.onload = loadImage;
+
+car.src = "../graphics/car.png";
+carWidth = car.width,
+  carHeight = car.height;
+
+function loadImage() {
+  imgWidth = scrollImg.width * imgScale,
+    imgHeight = scrollImg.height * imgScale;
+  canvasTemp.width = imgWidth;
+  canvasTemp.height = imgHeight;
+  render();
+}
+document.getElementById("container")?.addEventListener("click", function (e) {
+  const data = ctx.getImageData(e.x, e.y, 4, 4).data;
+
+  let RED = data[0]
+  let GREEN = data[1]
+  let BLUE = data[2]
+  let ALPHA = data[3]
+  console.log(RED + "," + GREEN + "," + BLUE, "")
+})
+
+
+function render() {
+
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+
+
+  if (scrollVal >= imgHeight - canvasHeight) {
+    scrollVal = 0;
+  }
+
+
+  scrollVal += speed;
+
+  ctx.drawImage(scrollImg, 0, (-imgHeight + canvasHeight) + scrollVal, imgWidth, imgHeight);
+  ctx.drawImage(car, driven, 0, carWidth, carHeight);
+  setTimeout(function () { render(); }, 10);
+}
+
+document.addEventListener("keypress", function (e) {
+  console.log(e)
+  if (e.code == "KeyA") {
+    driven = driven - 10;
+  }
+})
